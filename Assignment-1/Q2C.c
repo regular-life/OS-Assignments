4df1cfd5882c821fcff52561f6d819ec7231763f
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-08-27 23:56:08 +0530

    Update Q2C.c

--- Assignment-1/Q2C.c
+++ Assignment-1/Q2C.c
@@ -23,24 +23,22 @@ signed main(int argc, char *argv[])
     {
         filename = argv[2] ;
         struct stat filestat ;
-        if(stat(argv[1],&filestat) < 0)
+        if(stat(argv[2],&filestat) < 0)
         {
             return 1 ;
         }
         char *last_time = ctime(&filestat.st_mtime) ;
-        struct tm *tm_info = localtime(&filestat.st_mtime) ;
+        time_t mtime = filestat.st_mtime;
 
+        char rfc_time[100]; 
+        strftime(rfc_time, sizeof(rfc_time), "%a, %d %b %Y %H:%M:%S %z", localtime(&mtime));
         if (strcmp(argv[1], "-d") == 0)
         {
-            char string_buffer[80] ;
-            strftime(string_buffer, sizeof(string_buffer), "%a %b %d %H:%M:%S %Y", tm_info) ;
-            printf("Last modified date and time of %s (STRING format): %s\n", filename, string_buffer) ;
+            printf("Last modified date and time of %s (STRING format): %s\n", filename, last_time) ;
         }
         else if (strcmp(argv[1], "-R") == 0)
         {
-            char rfc5322_buffer[80] ;
-            strftime(rfc5322_buffer, sizeof(rfc5322_buffer), "%a, %d %b %Y %H:%M:%S %z", tm_info) ;
-            printf("Last modified date and time of %s (RFC 5322 format): %s\n", filename, rfc5322_buffer) ;
+            printf("Last modified date and time of %s (RFC 5322 format): %s\n", filename, rfc_time) ;
         }
         else
         {
