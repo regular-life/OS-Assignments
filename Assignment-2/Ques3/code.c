e2fb8f69a4c62e4ff8438d1b68dbf55bed57b277
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-09-16 00:15:05 +0530

    Update code.c

--- Assignment-2/Ques3/code.c
+++ Assignment-2/Ques3/code.c
@@ -3,6 +3,7 @@
 #include <linux/signal.h>
 #include <linux/sched.h>
 #include <linux/printk.h>
+#include <linux/init.h>
 
 MODULE_AUTHOR("Yash") ;
 MODULE_LICENSE("GPL") ;
@@ -10,9 +11,9 @@ MODULE_DESCRIPTION("Module to count running processes.") ;
 
 static long long count_running_processes(void)
 {
-    struct task_struct *task ;
+    struct task_struct *task = current ;
+    for (; task->pid != 1 ; task = task->parent) ;
     long long cnt = 0 ;
-    
     for_each_node(task)
     {
         cnt ++ ;
@@ -24,11 +25,11 @@ static long long count_running_processes(void)
 int init_module(void) 
 { 
     long long cnt = count_running_processes() ;
-    pr_info("Number of running process: %lld\n", cnt) ;
+    pr_info("Module loaded successfully.\nNumber of running process: %lld\n", cnt) ;
     return 0 ;
 }
 
 void cleanup_module(void) 
 {
-    pr_info("Module unloaded.\n") ;
+    pr_info("Module unloaded successfully.\n") ;
 } 
