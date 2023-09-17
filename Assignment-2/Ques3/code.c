aed3023b3495e48f001b76034df4f10f37649b92
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-09-17 18:19:55 +0530

    Update code.c

--- Assignment-2/Ques3/code.c
+++ Assignment-2/Ques3/code.c
@@ -15,27 +15,35 @@ MODULE_DESCRIPTION("Module to count running processes.") ;
 static long long count_running_processes(void)
 {
     long long cnt = 0 ;
-    for (int i = 0 ; i <= 32768 ; i ++)
+    rcu_read_lock() ;
+    for (int i = 0 ; i <= 100000000 ; i ++)
     {
         struct task_struct *task = pid_task(find_vpid(i), PIDTYPE_PID) ;
         //check if process is running
         if (task)
         {
-            cnt++;
-            //pr_info("Process %s with pid %d is running.\n", task->comm, task->pid) ;
+            if (task->__state == 0)
+            {
+                cnt ++ ;
+                printk(KERN_INFO "%s %d \n", task->comm, task->pid) ;
+            }
         }
     }
+    rcu_read_unlock() ;
     return cnt ;
 }
 
-int init_module(void) 
+static int __init display_init(void)
 { 
     long long cnt = count_running_processes() ;
-    pr_info("Module loaded successfully.\nNumber of running process: %lld\n", cnt) ;
+    printk(KERN_INFO "Module loaded successfully.\nNumber of running process: %lld\n", cnt) ;
     return 0 ;
 }
 
-void cleanup_module(void) 
+static void __exit display_cleanup(void)
 {
-    pr_info("Module unloaded successfully.\n") ;
-} 
+    printk(KERN_INFO "Module unloaded successfully.\n") ;
+}
+
+module_init(display_init) ;
+module_exit(display_cleanup) ;
