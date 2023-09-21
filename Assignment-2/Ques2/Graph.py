6937263d47ca0cc9d9d21a75416652b41116dfdc
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-09-21 19:47:04 +0530

    Update Graph.py

--- Assignment-2/Ques2/Graph.py
+++ Assignment-2/Ques2/Graph.py
@@ -6,17 +6,17 @@ list_values = {'SCHED_FIFO': [], 'SCHED_RR': [], 'SCHED_OTHER': []}
 for x in f:
     arr = x.split()
     if arr[0] in list_values:
-        list_values[arr[0]].append(float(arr[1]))
+        list_values[arr[0]] = float(arr[1])
 
 f.close()
 
-x_labels = range(len(list_values['SCHED_FIFO']))
+x_labels = range(1)
 
 plt.plot(x_labels, list_values['SCHED_FIFO'], label='SCHED_FIFO')
 plt.plot(x_labels, list_values['SCHED_RR'], label='SCHED_RR')
 plt.plot(x_labels, list_values['SCHED_OTHER'], label='SCHED_OTHER')
 
-plt.xlabel('Index')
+plt.xlabel('Scheduling Policy')
 plt.ylabel('Time')
 plt.legend()
 
