517c57c4cdf64653495d3f16b9d3b36431b1dea8
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-11-21 19:33:57 +0530

    only need to add perror in all 3 questions

--- Assignment-4/Ques3/Main.c
+++ Assignment-4/Ques3/Main.c
@@ -1,55 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
 
-// make a struct for queue
-typedef struct queue
-{
-    int index;
-    pthread_t thr;
-    int direction;
-    struct queue *next;
-} Queue;
+#define LEFT -1
+#define RIGHT 1
 
-sem_t left_sem[5], right_sem[5];
+#define MAX_CARS 5
 
-void *left(void *args)
+int total_left, total_right ;
+int left_count = 0, right_count = 0 ;
+sem_t on_bridge ;
+
+int curr_dir = 0 ;
+
+void* left(void* args)
 {
+    int id = (int)args ;
+    if (curr_dir == 0 || curr_dir == LEFT)
+    {
+        curr_dir = LEFT ;
+        sem_wait(&on_bridge) ;
+        crossing(LEFT, id) ;
+        sem_post(&on_bridge) ;
+    }
+    else
+    {
+        while (right_count != total_right) ;
+        curr_dir = LEFT ;
+        sem_wait(&on_bridge) ;
+        crossing(LEFT, id) ;
+        sem_post(&on_bridge) ;
+    }
 }
 
-void *right(void *args)
+void* right(void* args)
 {
+    int id = (int)args ;
+    if (curr_dir == 0 || curr_dir == RIGHT)
+    {
+        curr_dir = RIGHT ;
+        sem_wait(&on_bridge) ;
+        crossing(RIGHT, id) ;
+        sem_post(&on_bridge) ;
+    }
+    else
+    {
+        while (left_count != total_left) ;
+        curr_dir = RIGHT ;
+        sem_wait(&on_bridge) ;
+        crossing(RIGHT, id) ;
+        sem_post(&on_bridge) ;
+    }
 }
 
-void passing(int dir)
+void crossing(int dir, int id)
 {
+    printf("Car %d is crossing the bridge from %s\n", id, dir == LEFT ? "left" : "right") ;
+    sleep(2) ;
+    printf("Car %d has crossed the bridge from %s\n", id, dir == LEFT ? "left" : "right") ;
+    if (dir == LEFT)
+    {
+        left_count++ ;
+    }
+    else if (dir == RIGHT)
+    {
+        right_count++ ;
+    }
 }
 
 signed main()
 {
-    int left_cars, right_cars;
-    printf("Enter the number of cars on left: ");
-    scanf("%d", &left_cars);
-    printf("Enter the number of cars on right: ");
-    scanf("%d", &right_cars);
-    Queue *Q_left;
-    Queue *Q_right;
-    pthread_t left_thread[left_cars], right_thread[right_cars];
+    printf("Enter total number of cars from left: ");
+    scanf("%d", &total_left);
+    printf("Enter total number of cars from right: ");
+    scanf("%d", &total_right);
 
-    for (int i = 0; i < left_cars; i++)
+    sem_init(&on_bridge, 0, MAX_CARS) ;
+
+    pthread_t left_thread[total_left], right_thread[total_right];
+
+    for (int i = 0; i < total_left; i++)
     {
-        void *arr1[2];
-        arr1[0] = (void *)Q_left;
-        arr1[1] = (void *)i;
-        pthread_create(&left_thread[i], NULL, left, (void *)arr1);
+        pthread_create(&left_thread[i], NULL, left, (void*)i);
     }
-    for (int i = 0; i < right_cars; i++)
+    for (int i = 0; i < total_right; i++)
     {
-        void *arr2[2];
-        arr2[0] = (void *)Q_right;
-        arr2[1] = (void *)i;
-        pthread_create(&right_thread[i], NULL, right, (void *)arr2);
+        pthread_create(&right_thread[i], NULL, right, (void*)i);
     }
+
+    for (int i = 0; i < total_left; i++)
+    {
+        pthread_join(left_thread[i], NULL);
+    }
+    for (int i = 0; i < total_right; i++)
+    {
+        pthread_join(right_thread[i], NULL);
+    }
+    
+    sem_destroy(&on_bridge) ;
+
+    return 0;
 }
